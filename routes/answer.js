c9b8e41ff459cf92d074364acfb463c1faf1c83a
litsynp <nocte55is@gmail.com>
2020-11-15 17:18:51 +0900

    docs: correct a misleading comment in answer routes

--- routes/answer.js
+++ routes/answer.js
@@ -98,7 +98,7 @@ router.get("/", isLoggedIn, async (req, res, next) => {
       };
     }
 
-    // 문제 목록 조회
+    // 정답 목록 조회
     const answers = await Answer.findAndCountAll(queryOptions);
 
     // Get likes and dislikes
