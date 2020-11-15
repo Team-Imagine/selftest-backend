2540c9185a479aa51cb0a8b4f943364ed1d32ee0
litsynp <nocte55is@gmail.com>
2020-11-15 22:07:00 +0900

    fix: fix bugs from typos in getting evaluations
    This commit changes typos where "AVG" is written as "COUNT", for getting freshness and difficulty information of a question.

--- routes/bin/get_evaluations.js
+++ routes/bin/get_evaluations.js
@@ -25,7 +25,7 @@ module.exports.get_dislikes = async function (likeable_entity_id) {
 
 module.exports.get_average_difficulty = async function (question_id) {
   const average_difficulty = await Difficulty.findAll({
-    attributes: [[sequelize.fn("COUNT", sequelize.col("score")), "average_difficulty"]],
+    attributes: [[sequelize.fn("AVG", sequelize.col("score")), "average_difficulty"]],
     where: {
       question_id,
     },
@@ -37,12 +37,13 @@ module.exports.get_average_difficulty = async function (question_id) {
 
 module.exports.get_average_freshness = async function (question_id) {
   const average_freshness = await Freshness.findAll({
-    attributes: [[sequelize.fn("COUNT", sequelize.col("fresh")), "average_freshness"]],
+    attributes: [[sequelize.fn("AVG", sequelize.col("fresh")), "average_freshness"]],
     where: {
       question_id,
     },
     raw: true,
   });
 
+  console.log(average_freshness);
   return average_freshness[0];
 };
